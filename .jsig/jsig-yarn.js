25f9ea6a57a0d42e6df6980fc99b9b29f1812c86
Ash Ward <421038+ashward@users.noreply.github.com>
2020-05-15 13:33:42 +0100

    Initial commit

---
+++ .jsig/jsig-yarn.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-var */
+"use strict";
+
+var ChildProcess = require("child_process");
+var Path = require("path");
+var jsig = require(".");
+
+console.info(`👮‍♀️   JSInstallGuard: Using JSIG version: ${jsig.version}`);
+
+var rootDir = process.cwd();
+
+// Work out where the actual yarn installation is
+var yarnDir;
+
+try {
+  yarnDir = Path.dirname(ChildProcess.execSync("which yarn").toString());
+} catch (e) {
+  //  TODO: Add a way of specifying the yarn executable
+  console.error(
+    "🛑   JSInstallGuard: Could not find yarn executable using 'which yarn'"
+  );
+  process.exit(1);
+}
+
+// Get the list of allowed scripts
+var allowed;
+
+try {
+  allowed = require(Path.resolve(rootDir, "jsig-allow.json"));
+} catch (e) {
+  console.warn(
+    "❗   JSInstallGuard: Could not parse 'jsig-allow.json'. All install scripts will be blocked."
+  );
+  console.warn("❗   JSInstallGuard: ", e.message);
+  allowed = { allow: [] };
+}
+
+(function() {
+  // Keep a handle to the original spawn method
+  var originalSpawn = ChildProcess.spawn;
+
+  // We intercept the 'child_process.spawn()' function call so we can check
+  // whether it's in the allow list. If not we throw an error.
+  ChildProcess.spawn = function (command, args, options) {
+    if (arguments[2].env.npm_lifecycle_event) {
+      let path = arguments[2].cwd;
+
+      if (path.startsWith(rootDir)) {
+        path = path.substring(rootDir.length + 1);
+      }
+      var cmd = arguments[0];
+      var version = arguments[2].env.npm_package_version;
+
+      if (
+        allowed.allow.filter((allow) => {
+          return allow.path == path && allow.cmd == cmd && allow.v == version;
+        }).length == 0
+      ) {
+        console.error();
+        console.error();
+        console.error(
+          "============================== JSInstallGuard ===================================="
+        );
+        console.error(
+          "🚨   A Node module is attempting to execute a shell command which has been blocked:"
+        );
+        console.error(`🚨   Module: ${path}`);
+        console.error(`🚨   Version: ${version}`);
+        console.error(`🚨   Command:`);
+        console.error();
+        console.error(cmd);
+        console.error();
+        console.error(
+          "🚨   If you trust it (and make sure you actually understand and trust what it's doing)"
+        );
+        console.error(
+          "🚨   then add the following to the 'allow' array in jsig-allow.json to allow:"
+        );
+        console.error();
+        console.error(
+          JSON.stringify({ jsig: jsig.version, path: path, v: version, cmd: cmd })
+        );
+        console.error();
+        console.error(
+          "🚨   If you suspect this to be malicious activity then please report it."
+        );
+        console.error(
+          "🚨   https://docs.npmjs.com/reporting-a-vulnerability-in-an-npm-package"
+        );
+        console.error(
+          "=================================================================================="
+        );
+        console.error();
+
+        // We will throw an error rather than just silently not running the script.
+        throw new Error(`JSInstallGuard: Install script blocked for ${path}`);
+      }
+
+      return originalSpawn.call(this, command, args, options);
+    }
+  };
+})();
+
+require(Path.resolve(yarnDir, "yarn.js"));
---
+++ README.md
@@ -0,0 +1,54 @@
+# JSInstallGuard
+
+JSInstallGuard is a small security wrapper around the package manager which intercepts any `preinstall` and `postinstall` scripts.
+
+These are checked against an allow list, and if they have not been explicitly allowed then the install script will be blocked.
+
+Note: Currently only `yarn` is supported, but `npm` support will be forthcoming!
+
+## Installation (for Yarn)
+Because JSInstallGuard needs to be in place before the packages are installed, it needs to be manually copied into your project directory, ideally before your very first `yarn install` or `yarn add`.
+
+### 1. Download the code as a zip
+
+You can get the latest from: https://github.com/ashward/jsinstallguard/archive/master.zip
+
+> (I know this is the master branch, but I will add versioning and proper releases soon!)
+
+### 2. Unpack the zip into the root of your project.
+
+This will add:
+
+* A `.jsig/` directory which contains the JSInstallGuard code (feel free to inspect it and make sure you trust what it's doing.)
+* A `.yarnrc` file which will ensure that it's actually JSInstallGuard that runs when you run `yarn`
+* A `jsig-allow.json` file which contains an array of the allowed scripts. Add an entry to the `allow` array to allow a trusted script to run.
+
+If you already have a `.yarnrc` file then you will need to manually merge it.
+
+### 3. Check it's working
+
+Run
+
+```bash
+yarn --version
+```
+
+If it's working then you will see the following at the top of the output:
+
+`👮‍♀️   JSInstallGuard: Using JSIG version: x.x.x`
+
+If you see the above you can start installing packages!
+
+### 4. Install your packages
+
+When you install or add packages, when one tries to run an install script then you will see an error.
+
+You should then do what you need to do to make sure you trust the script it's running.
+
+If you do trust it then add the given line to the `allow: []` array in `jsig-allow.json`.
+
+If you find something potentially dodgy or malicious then please report it to npmjs: https://docs.npmjs.com/reporting-a-vulnerability-in-an-npm-package
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
